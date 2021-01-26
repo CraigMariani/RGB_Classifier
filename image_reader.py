8028317023625eac05ea656012c8d9d7bcf0bbfe
CraigMariani <craigmariani47@gmail.com>
2021-01-25 23:15:41 -0800

    image reader works

--- image_reader.py
+++ image_reader.py
@@ -14,17 +14,32 @@ class ImageReader:
     def read_directory(self):
         path = self.path
         
+        rgb_vals = []
         for file in path.iterdir():
-            f=open(file,'r')
-    
+            # f=open(file,'r')
+            
+            # Open Image
+            im = Image.open(file)
+            # Quantize down to 2 colour palettised image using *"Fast Octree"* method:
+            q = im.quantize(colors=1)
+
+            # Now look at the first 2 colours, each 3 RGB entries in the palette:
+            rgb_val = tuple(q.getpalette()[:3])    
+            rgb_vals.append(rgb_val)    
+        return rgb_vals
+
     def create_colors(self):
-        path = self.path 
 
         for i in range(10):
-            Image.new(mode='RGB', color)
-        # subprocess.call(['cd', 'colors/'], shell=True)
-        # subprocess.call('ls', shell=True)
+            im = Image.new(mode='RGB', size=(100, 100), color=(
+                int(np.random.uniform(0,1) * 255),
+                int(np.random.uniform(0,1) * 255),
+                int(np.random.uniform(0,1) * 255)
+                ))
+            color_name = 'colors/color_{}.png'.format(str(i))
+            im.save(color_name)
     
 if __name__ == '__main__':
     im = ImageReader() 
-    im.create_colors()
+    # im.create_colors()
+    print(im.read_directory())
